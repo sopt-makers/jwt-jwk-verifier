795b9c4e892705e191d789f10aaa947bf8a2babd
sung-silver <jinna0319@gmail.com>
2025-04-07 16:10:53 +0900

    add: 필요한 의존성 추가
    - JWK (JWKSet, RSAKey 등 JWK 파싱용)
    - caffeine (JWK 키 캐싱)]
    - logging
    - lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
+	implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+	implementation("org.slf4j:slf4j-api:2.0.9")
+	implementation("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
