ea352363221ad07db1c4fbdb6a3078fa2ec97143
sung-silver <jinna0319@gmail.com>
2025-04-07 15:26:50 +0900

    init: initialize project

---
+++ src/main/java/sopt/makers/jwt/verifier/Application.java
@@ -0,0 +1,13 @@
+package sopt.makers.jwt.verifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
