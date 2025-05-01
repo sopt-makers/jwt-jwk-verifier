7fea884f1dc3a6f71d8bebd394215358235506f1
sung-silver <jinna0319@gmail.com>
2025-05-01 17:55:11 +0900

    feat: makers authentication 구현

---
+++ src/main/java/sopt/makers/jwt/verifier/security/authentication/MakersAuthentication.java
@@ -0,0 +1,66 @@
+package sopt.makers.jwt.verifier.security.authentication;
+
+import lombok.Getter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class MakersAuthentication implements Authentication {
+
+    private final String userId;
+    private final List<GrantedAuthority> authorities;
+    private boolean authenticated;
+
+    public MakersAuthentication(String userId, List<String> roles) {
+        this.userId = userId;
+        this.authorities = roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getRoles() {
+        return authorities.stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null; // JWT 기반 인증이라 자격 증명 X
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) throws IllegalArgumentException {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getName() {
+        return userId;
+    }
+}
