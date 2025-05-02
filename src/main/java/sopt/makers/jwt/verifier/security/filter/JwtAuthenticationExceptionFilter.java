67ca6beaec57bdf808e199557b52719a01778ca2
sung-silver <jinna0319@gmail.com>
2025-05-02 20:11:22 +0900

    feat: JwtAuthenticationExceptionFilter 구현

---
+++ src/main/java/sopt/makers/jwt/verifier/security/filter/JwtAuthenticationExceptionFilter.java
@@ -0,0 +1,30 @@
+package sopt.makers.jwt.verifier.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import sopt.makers.jwt.verifier.exception.JwtException;
+import sopt.makers.jwt.verifier.util.ResponseUtil;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationExceptionFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+      @NonNull final HttpServletRequest request,
+      @NonNull final HttpServletResponse response,
+      @NonNull final FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      filterChain.doFilter(request, response);
+    } catch (JwtException e) {
+      ResponseUtil.generateErrorResponse(response, e);
+    }
+  }
+}
