6519c9e2b393ddf7ea6bdd97c3f553d40ca35178
sung-silver <jinna0319@gmail.com>
2025-05-09 15:39:59 +0900

    [FEAT] 인증 헤더 없음에 대한 예외처리 추가

--- src/main/java/sopt/makers/jwt/verifier/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/sopt/makers/jwt/verifier/security/filter/JwtAuthenticationFilter.java
@@ -10,35 +10,41 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import sopt.makers.jwt.verifier.jwt.exception.JwtException;
 import sopt.makers.jwt.verifier.jwt.service.JwtAuthenticationService;
 import sopt.makers.jwt.verifier.security.authentication.MakersAuthentication;
 
 import java.io.IOException;
 
+import static sopt.makers.jwt.verifier.jwt.code.JwtFailure.JWT_MISSING_AUTH_HEADER;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtAuthenticationService jwtAuthenticationService;
-  private static final int ACCESS_TOKEN_PREFIX = 7;
+    private final JwtAuthenticationService jwtAuthenticationService;
+    private static final String ACCESS_TOKEN_PREFIX = "Bearer ";
 
-  @Override
-  protected void doFilterInternal(
-      @NonNull final HttpServletRequest request,
-      @NonNull final HttpServletResponse response,
-      @NonNull final FilterChain filterChain)
-      throws ServletException, IOException {
-    String authorizationToken = getAuthorizationToken(request);
-    MakersAuthentication authentication = jwtAuthenticationService.authenticate(authorizationToken);
+    @Override
+    protected void doFilterInternal(
+            @NonNull final HttpServletRequest request,
+            @NonNull final HttpServletResponse response,
+            @NonNull final FilterChain filterChain)
+            throws ServletException, IOException {
+        String authorizationToken = getAuthorizationToken(request);
+        MakersAuthentication authentication = jwtAuthenticationService.authenticate(authorizationToken);
 
-    authentication.setAuthenticated(true);
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    filterChain.doFilter(request, response);
-  }
+        authentication.setAuthenticated(true);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
 
-  private String getAuthorizationToken(final HttpServletRequest request) {
-    String authorizationHeaderValue =
-        request.getHeader(HttpHeaders.AUTHORIZATION).substring(ACCESS_TOKEN_PREFIX);
-    return authorizationHeaderValue.trim();
-  }
+    private String getAuthorizationToken(final HttpServletRequest request) {
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header == null || !header.startsWith(ACCESS_TOKEN_PREFIX)) {
+            throw new JwtException(JWT_MISSING_AUTH_HEADER);
+        }
+        return header.substring(ACCESS_TOKEN_PREFIX.length()).trim();
+    }
 }
