1ded57157289d8332b8e872036265fff733717e8
sung-silver <jinna0319@gmail.com>
2025-05-10 01:21:16 +0900

    refactor: 사용되지 않는 import문 제거

--- src/main/java/sopt/makers/jwt/verifier/security/authentication/MakersAuthentication.java
+++ src/main/java/sopt/makers/jwt/verifier/security/authentication/MakersAuthentication.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 public class MakersAuthentication implements Authentication {
