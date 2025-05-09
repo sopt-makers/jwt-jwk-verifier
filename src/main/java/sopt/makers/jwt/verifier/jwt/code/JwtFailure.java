acb3cc86eacf59e51136420531d1ec841e59d992
sung-silver <jinna0319@gmail.com>
2025-05-09 15:39:59 +0900

    [FEAT] 인증 헤더 없음에 대한 예외처리 추가

--- src/main/java/sopt/makers/jwt/verifier/jwt/code/JwtFailure.java
+++ src/main/java/sopt/makers/jwt/verifier/jwt/code/JwtFailure.java
@@ -1,18 +1,21 @@
 package sopt.makers.jwt.verifier.jwt.code;
 
-import static lombok.AccessLevel.*;
+import static lombok.AccessLevel.PRIVATE;
 
-import lombok.*;
-import org.springframework.http.*;
-import sopt.makers.jwt.verifier.code.base.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import sopt.makers.jwt.verifier.code.base.FailureCode;
 
 @Getter
 @RequiredArgsConstructor(access = PRIVATE)
 public enum JwtFailure implements FailureCode {
-  JWT_PARSE_FAILED(HttpStatus.BAD_REQUEST, "잘못된 형식의 JWT입니다."),
-  JWT_INVALID_CLAIMS(HttpStatus.UNAUTHORIZED, "JWT의 클레임이 유효하지 않습니다."),
-  JWT_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "JWT 검증에 실패했습니다.");
+    JWT_MISSING_AUTH_HEADER(HttpStatus.UNAUTHORIZED, "인증 헤더가 존재하지 않습니다."),
+    JWT_PARSE_FAILED(HttpStatus.UNAUTHORIZED, "잘못된 형식의 JWT입니다."),
+    JWT_INVALID_CLAIMS(HttpStatus.UNAUTHORIZED, "JWT의 클레임이 유효하지 않습니다."),
+    JWT_VERIFICATION_FAILED(HttpStatus.UNAUTHORIZED, "JWT 검증에 실패했습니다.");
 
-  private final HttpStatus status;
-  private final String message;
+    private final HttpStatus status;
+    private final String message;
 }
